641610291cb4f6cc17ad063bdcb932a4895e82a0
jrking365 <nigoumijeanroger@gmail.com>
2019-07-21 12:13:12 -0400

    filter todos

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div class="container">
       <AddTodos />
+      <FilterTodos />
       <Todos />
     </div>
   </div>
@@ -10,12 +11,14 @@
 <script>
 import Todos from "./components/Todos.vue";
 import AddTodos from "./components/AddTodos.vue";
+import FilterTodos from "./components/FilterTodos.vue";
 
 export default {
   name: "app",
   components: {
     Todos,
-    AddTodos
+    AddTodos,
+    FilterTodos
   }
 };
 </script>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -9,8 +9,9 @@ const getters = {
 };
 
 const actions = {
-    async fetchTodos({ commit }) {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+    async fetchTodos({ commit }, limit) {
+
+        const response = limit ? await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`) : await axios.get('https://jsonplaceholder.typicode.com/todos');
 
         commit('setTodos', response.data);
     },
@@ -23,6 +24,13 @@ const actions = {
     async deleteTodo({ commit }, id) {
         await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
         commit('removeTodo', id);
+    },
+    async filterTodos({ commit }, e) {
+        // Get selected number
+        const limit = parseInt(e.target.options[e.target.options.selectedIndex].innerText);
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`);
+
+        commit('setTodos', response.data);
     }
 };
 
